6a55e63a56e440ee7d01168e9e2d4543d6c2c980
evanjyli <evanli2011@hotmail.com>
2025-12-15 11:58:10 -0800

    vip traces

---
+++ output_vip/sim.scripts.py
@@ -0,0 +1,5 @@
+import sys
+sys.argv = [ "/home/evanjyli/sniper-7.4/scripts/periodic-stats.py", "1000:2000" ]
+execfile("/home/evanjyli/sniper-7.4/scripts/periodic-stats.py")
+sys.argv = [ "/home/evanjyli/sniper-7.4/scripts/markers.py", "markers" ]
+execfile("/home/evanjyli/sniper-7.4/scripts/markers.py")
